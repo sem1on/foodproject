8660ea871224bed87fc1b49d7513c2be4aa66e59
sem1on <sgorlov21@gmail.com>
2021-11-16 20:29:44 +0300

    Добавление слайдера, простой вариант

--- js/script.js
+++ js/script.js
@@ -284,4 +284,59 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000);
     }
+
+    // Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+            prev = document.querySelector('.offer__slider-prev'),
+            next = document.querySelector('.offer__slider-next'),
+            total = document.querySelector('#total'),
+            current = document.querySelector('#current');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+    
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        showMySlide(slideIndex, slides);
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+
+    }
+
+    function showMySlide (index, arr) {
+        arr.forEach((item) => {
+            item.classList.add('hide');
+        });
+        arr[index-1].classList.add('show');
+        arr[index-1].classList.remove('hide');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+            
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+
 });
